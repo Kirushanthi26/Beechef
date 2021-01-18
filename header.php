8e4f2b9864f85f7701128b04d7f3e8919b29a28d
kirushanthi <kirushanthikiru26@gmail.com>
2021-01-18 15:52:57 +0530

    link that css file to hader file

--- header.php
+++ header.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beechef</title>
-    <!-- <link rel="stylesheet" href="css/style.css" type="text/css"> -->
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" >
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" ></script>
     <!--create header part for login page-->
+    <link rel="stylesheet" href="css/login_Style.css">
 </head>
 <body>
