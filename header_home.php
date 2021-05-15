16322d869dc1f24e3886597b78c2252d0446774e
kirushanthi <kirushanthikiru26@gmail.com>
2021-05-15 13:13:23 +0530

    basic code history.php

--- header_home.php
+++ header_home.php
@@ -10,14 +10,14 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!--create header part for login page-->
     <link rel="stylesheet" href="css/homestyle.css">
-
+    <link rel="stylesheet" href="css/history.css">
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     
-
+    
 
 
 </head>
--- history.php
+++ history.php
@@ -4,7 +4,14 @@ include "database.php";
 include "header_home.php";
 include "headerdesign.php";?>
 
-
+<div class="history__container">
+    <div class="history__content_box">
+    <h1>Your Order History</h1>
+        <div class="history__details_box">
+            
+        </div>
+    </div>
+</div>
 
 <!-- footer -->
 <?php include "footer.php";?>
